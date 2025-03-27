02d57b340c113d1c9d821826fdb1a812e981276e
Tam Vo <tamvoforwork@gmail.com>
2025-03-27 16:17:37 +0700

    Update: Cloudfront HTTPS

--- 04-cloudfront.tf
+++ 04-cloudfront.tf
@@ -71,6 +71,21 @@ resource "aws_cloudfront_distribution" "alb_distribution" {
     cache_policy_id          = aws_cloudfront_cache_policy.production.id
   }
 
+  ordered_cache_behavior {
+    path_pattern           = "*"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = aws_alb.starcamp_alb.id
+    viewer_protocol_policy = "redirect-to-https"
+
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.production.id
+    cache_policy_id          = aws_cloudfront_cache_policy.production.id
+
+    default_ttl = 86400
+    min_ttl     = 0
+    max_ttl     = 31536000
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
