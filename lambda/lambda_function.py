f17ffeec02d578eaef68b597cfaf3e5cc2e07241
Tam Vo <tamvoforwork@gmail.com>
2025-03-30 14:48:10 +0700

    Update lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,10 +3,11 @@ import json
 
 def lambda_handler(event, context):
     try:
+        email = event["email"]
         endpoint = '14.225.210.134'
         username = 'root'
         password = 'thegatekeeper'
-        database_name = 'users'
+        database_name = 'workshop'
         port = 3307  # Use an integer for the port
 
         # Attempt to connect to the database
@@ -17,13 +18,31 @@ def lambda_handler(event, context):
             database=database_name,
             port=port
         )
+        
+        with connection.cursor() as cursor:
+            # Select a user by email
+            select_query = "SELECT description FROM users WHERE email = %s;"
+            cursor.execute(select_query, (email,))
+            result = cursor.fetchall()
+            print(type(result))
+            print(result)
+            if not result:
+                return {
+                    "statusCode": 200,
+                    "body": json.dumps("This email does not exist")
+                }
+            # Print the results
+            description = None
+            for row in result:
+                description = row[0]
+            
 
         # If the connection is successful, return a success message
         connection.close()
         
         return {
             "statusCode": 200,
-            "body": json.dumps("Connection successful!")
+            "body": json.dumps(f"{description}")
         }
     
     except Exception as e:
