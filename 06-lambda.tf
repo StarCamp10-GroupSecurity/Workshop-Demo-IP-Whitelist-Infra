513c584885daf8f7ec797d34ba1b380648ef5995
Tam Vo <tamvoforwork@gmail.com>
2025-03-30 09:39:36 +0700

    Update: lambda connection done to other db server

---
+++ 06-lambda.tf
@@ -0,0 +1,55 @@
+# main.tf
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_iam_role" "lambda_role" {
+    name = "lambda_execution_role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Principal = {
+                Service = "lambda.amazonaws.com"
+            }
+            Effect = "Allow"
+        }]
+    })
+}
+
+resource "aws_iam_policy" "lambda_cloudwatch_logs" {
+    name        = "LambdaCloudWatchLogsPolicy"
+    description = "Policy for Lambda to write logs to CloudWatch"
+    
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = [
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"
+                ]
+                Resource = "*"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logging" {
+    policy_arn = aws_iam_policy.lambda_cloudwatch_logs.arn
+    role       = aws_iam_role.lambda_role.name  # Replace with your Lambda role name
+}
+
+resource "aws_lambda_function" "my_lambda" {
+    function_name = "my_lambda_function"
+    handler       = "lambda_function.lambda_handler"
+    runtime       = "python3.10"
+
+    role          = aws_iam_role.lambda_role.arn
+    filename      = "lambda_function.zip"
+
+    source_code_hash = filebase64sha256("lambda_function.zip")
+}
---
+++ lambda/lambda_function.py
@@ -0,0 +1,33 @@
+import pymysql
+import json
+
+def lambda_handler(event, context):
+    try:
+        endpoint = '14.225.210.134'
+        username = 'root'
+        password = 'swinburnemexer'
+        database_name = 'mexer'
+        port = 3306  # Use an integer for the port
+
+        # Attempt to connect to the database
+        connection = pymysql.connect(
+            host=endpoint,
+            user=username,
+            password=password,
+            database=database_name,
+            port=port
+        )
+
+        # If the connection is successful, return a success message
+        connection.close()
+        
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Connection successful!")
+        }
+    
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})  # Serialize error message
+        }
