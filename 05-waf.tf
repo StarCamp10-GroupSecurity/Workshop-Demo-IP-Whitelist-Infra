1def2b4d05119ad97145c3f3f4d7ed2c7dd17e89
Tam Vo <tamvoforwork@gmail.com>
2025-04-08 18:06:57 +0700

    Update: Cloudfront

--- 05-waf.tf
+++ 05-waf.tf
@@ -1,53 +1,53 @@
-# # IP Whitelist set
-# resource "aws_wafv2_ip_set" "demo_whitelist_set" {
-#   name               = "demo_whitelist_set"
-#   description        = "IP set that in the Whitelisting list"
-#   scope              = "CLOUDFRONT"
-#   ip_address_version = "IPV4"
-#   addresses          = var.allow_ip_addresses
+# IP Whitelist set
+resource "aws_wafv2_ip_set" "demo_whitelist_set" {
+  name               = "demo_whitelist_set"
+  description        = "IP set that in the Whitelisting list"
+  scope              = "CLOUDFRONT"
+  ip_address_version = "IPV4"
+  addresses          = var.allow_ip_addresses
 
-#   tags = {
-#     Name = "Whitelist Set"
-#   }
-# }
+  tags = {
+    Name = "Whitelist Set"
+  }
+}
 
-# resource "aws_wafv2_web_acl" "web_acl" {
-#   name        = "example-web-acl"
-#   description = "Example Web ACL"
-#   scope       = "CLOUDFRONT"
-#   default_action {
-#     block {}
-#   }
+resource "aws_wafv2_web_acl" "web_acl" {
+  name        = "example-web-acl"
+  description = "Example Web ACL"
+  scope       = "CLOUDFRONT"
+  default_action {
+    block {}
+  }
 
-#   visibility_config {
-#     cloudwatch_metrics_enabled = false
-#     metric_name                = "web-acl-metric"
-#     sampled_requests_enabled   = false
-#   }
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "web-acl-metric"
+    sampled_requests_enabled   = false
+  }
 
-#   rule {
-#     name     = "allow-only-from-allowed-ips"
-#     priority = 1
-
-
-#     statement {
-#       ip_set_reference_statement {
-#         arn = aws_wafv2_ip_set.demo_whitelist_set.arn
-#       }
+  rule {
+    name     = "allow-only-from-allowed-ips"
+    priority = 1
 
 
-#     }
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.demo_whitelist_set.arn
+      }
 
-#     action {
-#       allow {}
-#     }
-#     visibility_config {
-#       cloudwatch_metrics_enabled = false
-#       metric_name                = "allow-only-from-allowed-ips-metric"
-#       sampled_requests_enabled   = false
-#     }
-#   }
-# }
+
+    }
+
+    action {
+      allow {}
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "allow-only-from-allowed-ips-metric"
+      sampled_requests_enabled   = false
+    }
+  }
+}
 
 # IP Whitelist set
 resource "aws_wafv2_ip_set" "demo_whitelist_set_api" {
